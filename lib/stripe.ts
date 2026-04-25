6c902fe2b6755b97e90a7d3c417169e5bdaa32a6
Michael Matthews <michael.matthews3232@gmail.com>
2026-04-25 10:34:04 -0600

    Fix Stripe API version to stable 2024-06-20

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-03-31.basil',
+  apiVersion: '2024-06-20',
 })
 
 export const SETUP_FEE_PRICE_ID   = process.env.STRIPE_SETUP_FEE_PRICE_ID!
